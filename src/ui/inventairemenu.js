8ba61959edf8965ad7963229498890058c813728
thyshimrod <thyshimrod@gmail.com>
2019-05-05 17:41:23 +0200

    design inventaire menu

--- src/ui/inventairemenu.js
+++ src/ui/inventairemenu.js
@@ -29,6 +29,32 @@ survivshim.InventaireMenu.prototype ={
             ctx.beginPath();
             ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
             ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+            ctx.font = "1Opx Arial";
+            ctx.fillStyle = survivshim.C.COLOR_TEXT;
+            let text = "Equipement";
+            ctx.fillText(text ,
+                this.x + 10, 
+                this.y + 15);            
+            text = "Materiaux";
+            ctx.fillText(text ,
+                this.x + 150, 
+                this.y + 15);            
+            text = "Consommable";
+            ctx.fillText(text ,
+                this.x + 300, 
+                this.y + 15);
+            ctx.beginPath();
+            ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
+            ctx.rect(this.x, this.y, this.width, 20);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
+            ctx.rect(this.x, this.y, 120, 20);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
+            ctx.rect(this.x, this.y, 250, 20);
             ctx.stroke();            
         }
     },
