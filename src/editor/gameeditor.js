d22c0d19c0c588ff3997777922b3280584f2ed58
thyshimrod <thyshimrod@gmail.com>
2019-05-20 11:25:34 +0200

    déplacer la carte en fonction de la position de la souris

--- src/editor/gameeditor.js
+++ src/editor/gameeditor.js
@@ -5,13 +5,14 @@ survivshim.GameEditor = function (){
     this.tileSize = survivshim.C.TILE_SIZE_PC;
     this.decalageX = 0;
     this.decalageY = 0;
+    this.clientX = 0;
+    this.clientY = 0;
 }
 
 survivshim.GameEditor.prototype ={
 
     gameLoop: function (){
        survivshim.levelEditor.render();
-
     },
 
     init : function(){
@@ -23,6 +24,21 @@ survivshim.GameEditor.prototype ={
         survivshim.levelEditor.init();
         this.centerX = window.innerWidth / 2 -  this.tileSize / 2 ;
         this.centerY = window.innerHeight / 2 - this.tileSize / 2 - 70;
+        var _this = this;
+        var ctx = survivshim.canvas.canvasMouse.addEventListener('mousemove', survivshim.gameEditor.clickEvent);
+    },
+
+    clickEvent : function(evt){
+        if (evt.pageX < 50){
+            survivshim.gameEditor.decalageX +=8;    
+        }else if (evt.pageX > (window.innerWidth-50)){
+            survivshim.gameEditor.decalageX -= 8;
+        }
+        if (evt.pageY < 50){
+            survivshim.gameEditor.decalageY +=8;    
+        }else if (evt.pageY > (window.innerHeight-50)){
+            survivshim.gameEditor.decalageY -= 8;
+        }
     }
 }
 
