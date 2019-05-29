f6c789639fd2bf990bf01d09efdc701c8d4adce6
thyshimrod <thyshimrod@gmail.com>
2019-05-29 20:23:07 +0200

    menu nouveau blueprint

--- src/ui/listblueprintmenu.js
+++ src/ui/listblueprintmenu.js
@@ -11,7 +11,9 @@ survivshim.ListBlueprintMenu = function (){
     this.ctx = null;
     this.canBuild = false;
     this.timerBuild = 0;
+    this.cellSize = 200
     this.listOfBP = [];
+    this.state = survivshim.C.STATE_LIST_BLUEPRINT_BLUEPRINTS;
 };
 
 survivshim.ListBlueprintMenu.prototype ={
@@ -38,10 +40,32 @@ survivshim.ListBlueprintMenu.prototype ={
         this.ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
         this.ctx.rect(this.x, this.y, this.width, this.height);
         this.ctx.stroke();
+        this.ctx.fillStyle = survivshim.C.COLOR_TEXT;
+        if (this.state === survivshim.C.STATE_LIST_BLUEPRINT_BLUEPRINTS){
+            this.ctx.fillStyle = survivshim.C.COLOR_TEXT_CHOOSEN;
+        }
         let text = "Blueprint";
         this.ctx.fillText(text ,
-            this.x + 10, 
-            this.y + 10);
+            this.x + 70, 
+            this.y + 15);
+        this.ctx.fillStyle = survivshim.C.COLOR_TEXT;
+        if (this.state === survivshim.C.STATE_LIST_BLUEPRINT_NEW){
+            this.ctx.fillStyle = survivshim.C.COLOR_TEXT_CHOOSEN;
+        }
+        text = "Nouveau";
+        this.ctx.fillText(text ,
+                this.x + 280, 
+                this.y + 15);
+
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
+        this.ctx.rect(this.x , this.y, this.cellSize, 20);
+        this.ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = survivshim.C.COLOR_TURQUOISE;
+        this.ctx.rect(this.x + this.cellSize, this.y, this.cellSize, 20);
+        this.ctx.stroke();
+        
     },
 
     renderBP : function(){
@@ -66,12 +90,18 @@ survivshim.ListBlueprintMenu.prototype ={
         })
     },
 
+    renderNewBP : function(){
 
+    },
 
     render : function(){
         if (this.active === true ){
             this.renderFrame();
-            this.renderBP();
+            if (this.state === survivshim.C.STATE_LIST_BLUEPRINT_BLUEPRINTS){
+                this.renderBP();
+            }else{
+                this.renderNewBP();
+            }
         }
     },
 
@@ -79,18 +109,24 @@ survivshim.ListBlueprintMenu.prototype ={
         if (this.active === true ){
             if(x < (this.x + this.width) && x > (this.x)
             && y < (this.y + this.height) && y > (this.y)){
-                var _x = x;
-                var _y = y;
-                var _this = this;
-                this.listOfBP.forEach(function(bp){
-                    if (_x > bp.x 
-                        && _x < (bp.x +32)
-                        && _y > bp.y
-                        && _y < (bp.y+32)){
-                            _this.hideMenu();
-                            survivshim.blueprintMenu.showMenu(bp.id);
-                        }
-                })
+                if ( x < (this.x + this.cellSize) && y < (this.y + 30)){
+                    this.state = survivshim.C.STATE_LIST_BLUEPRINT_BLUEPRINTS;
+                }else if (x > (this.x + this.cellSize) && y < (this.y + 30)){
+                    this.state = survivshim.C.STATE_LIST_BLUEPRINT_NEW;   
+                }else if (this.state === survivshim.C.STATE_LIST_BLUEPRINT_BLUEPRINTS){
+                    var _x = x;
+                    var _y = y;
+                    var _this = this;
+                    this.listOfBP.forEach(function(bp){
+                        if (_x > bp.x 
+                            && _x < (bp.x +32)
+                            && _y > bp.y
+                            && _y < (bp.y+32)){
+                                _this.hideMenu();
+                                survivshim.blueprintMenu.showMenu(bp.id);
+                            }
+                    })
+                }
                 return survivshim.C.CLICK_ON_WINDOW;
             }else{
                 this.hideMenu();
